eb002c5c855679a44917a040b0ed430ce6e757b0
Mike Gibbens <mike.gibbens@siemens.com>
2021-12-10 14:59:55 +0000

    removed dead code from 10b

--- 10b/tool_src/advent.py
+++ 10b/tool_src/advent.py
@@ -1,28 +1,20 @@
 import os
 
-
-def isCharacterN(stringValue,characterIndex,wantedChar):
-    return stringValue[characterIndex-1] == wantedChar
-
 def processLineOfInputIntoStruct(line,struct):
     struct.append(line.strip())
     
 def processInputFile(filePath):
-    
     readlines = []
-    
     if os.path.exists(filePath):
         f = open(filePath, "r")
         for x in f:
             processLineOfInputIntoStruct(x,readlines)
     else:
         print("File "+filePath+" not found")
-
     return readlines
 
 def score(str):
     assert(len(str)==1)
-    
     if str == "(": return 1
     if str == "[": return 2
     if str == "{": return 3
@@ -44,27 +36,6 @@ def isOnlyOpeningBrackets(str):
             return False
     return True
 
-def indexOfChar(str):
-    if str == "(": return 0
-    if str == "[": return 1
-    if str == "{": return 2
-    if str == "<": return 3
-    if str == ")": return 0
-    if str == "]": return 1
-    if str == "}": return 2
-    if str == ">": return 3
-    assert(False)
-    return -100
-
-def bClosesA(bracketA,bracketB):
-    assert(len(bracketA) == 1)
-    assert(len(bracketB) == 1)
-    if bracketA =='[' and bracketB ==']': return True
-    if bracketA =='(' and bracketB ==')': return True
-    if bracketA =='{' and bracketB =='}': return True
-    if bracketA =='<' and bracketB =='>': return True
-    return False
-
 def removeNeighbourPairs(line):
     str3= line.replace("<>","")
     str3= str3.replace("()","")
@@ -72,43 +43,33 @@ def removeNeighbourPairs(line):
     str3= str3.replace("[]","")
     return str3
 
-def firstIncorrectClosingOnLine(line):
-    for i in line:
-        if isClosingBracket(i):
-            return i
-    assert(False)
-
-
 def mainTask():
 
     input_path = "C:\\Users\\gibbens\\Documents\\Arduino\\AdventOfCode2021\\10b\\tool_src\\input.txt"
     struct = processInputFile(input_path)
 
     bracketCount = [0,0,0,0]
-
     scores = []
 
     for lineToParse in struct:
         print(lineToParse)
-
+        # shrink strings by removing 'empty' pairs of brackets e.g. ()
         currentLength = len(lineToParse)
         lineToParse = removeNeighbourPairs(lineToParse)
         newLength = len(lineToParse)
+        # keep trying to shrink string until length doesn't change
         while(newLength < currentLength):
             currentLength = len(lineToParse)
             lineToParse = removeNeighbourPairs(lineToParse)
-            #print(lineToParse)
             newLength = len(lineToParse)
-
         print("Finished shrinking")
+        
         if isOnlyOpeningBrackets(lineToParse):
             print("Line {} is just incomplete".format(lineToParse))
             lineScore = 0
             for i in lineToParse[::-1]:
-                # go backwards
                 lineScore = lineScore * 5
                 lineScore = lineScore+score(i)
-            print("line score {}".format(lineScore))
             scores.append(lineScore)
         
     scores.sort()
@@ -116,5 +77,4 @@ def mainTask():
     print( scores[int(len(scores)/2)])
 
 if __name__ == "__main__":
-
     mainTask()
