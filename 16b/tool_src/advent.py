123ba9c3927eb88601741f8472c62e4a5fb37037
Mike Gibbens <mike.gibbens@siemens.com>
2021-12-17 17:53:31 +0000

    Finally finished 17a after convincing myself the answer wasn't infinity.

---
+++ 16b/tool_src/advent.py
@@ -0,0 +1,330 @@
+import os
+from time import perf_counter, process_time
+import math
+
+class Packet:
+
+    def __init__(self,inputBitStream,startingIndex,depth):
+        
+        #Every packet begins with a standard header: 
+        # the first three bits encode the packet version, 
+        # and the next three bits encode the packet type ID. 
+        pvBStart = startingIndex
+        pvBEnd = pvBStart+3
+        packetVersionBitString = inputBitStream[pvBStart:pvBEnd]
+        ptIDBStart = pvBEnd
+        ptIDBEnd = ptIDBStart+3
+        packetTypeIDBitString =inputBitStream[ptIDBStart:ptIDBEnd]
+        
+        self.startIndex = startingIndex
+        self.endPosInBitStream = -1
+        self.packetVersion = int(packetVersionBitString,2)
+        self.typeID  = int(packetTypeIDBitString,2)
+        self.depth = depth
+
+        print("Stream position {} started new packet version {} typeID {} depth {}".format(startingIndex,self.packetVersion,self.typeID,depth))
+
+        if self.typeID == 4:
+            #Literal value packets encode a single binary number
+            self.subPackets = []
+            self.number=-1
+            self.eatLiteralFromStream(inputBitStream,ptIDBEnd)
+            print("Literal value {} - at depth {} uses up to bit {}".format(self.number,self.depth,self.endPosInBitStream))
+        
+        # Every other type of packet (any packet with a type ID other than 4) represent an operator that performs some calculation on
+        # one or more sub-packets contained within.
+        # Right now, the specific operations aren't important; focus on parsing the hierarchy of sub-packets.
+        else:
+            self.number=-1
+            self.subPackets = []
+            # an operator packet can use one of two modes indicated by the bit immediately after the packet header; this is called the length type ID
+            self.lengthType = 15 # If length type is '0' - total length in bits of the sub-packets contained by this packet
+            if inputBitStream[ptIDBEnd] == '1':
+                self.lengthType = 11 #  the number of sub-packets immediately contained by this packet
+
+            if self.lengthType == 11:
+                nSubPacketString = inputBitStream[ptIDBEnd+1:ptIDBEnd+12]
+                assert(len(nSubPacketString) == 11)
+                nSubpackets = int(nSubPacketString,2)
+                print("{} subpackets to read starting from {}".format(nSubpackets,ptIDBEnd+12))
+                self.endPosInBitStream = self.eatNSubPackets(inputBitStream,ptIDBEnd+12,nSubpackets,depth+1)
+            else:
+                assert(self.lengthType == 15)
+                nSubPacketLengthString = inputBitStream[ptIDBEnd+1:ptIDBEnd+16]
+                assert(len(nSubPacketLengthString) == 15)
+                nSubpacketLength = int(nSubPacketLengthString,2)
+                subpacketRegionStart = ptIDBEnd+16
+                subpacketRegionEnd = (subpacketRegionStart+nSubpacketLength)-1 # length includes the first character
+                print("Unknown number of subpackets to be read, using {} bits from {} to {}".format(nSubpacketLength,subpacketRegionStart,subpacketRegionEnd))
+                self.endPosInBitStream = self.eatSubPacketsForNBits(inputBitStream,ptIDBEnd+16,nSubpacketLength,depth+1)
+                assert(self.endPosInBitStream == subpacketRegionEnd)
+
+    def eatSubPacketsForNBits(self,inputBitStream,startingIndex,nBits,depth):
+        # Don't know how many subpackets there will be, but process next one until we has consumed enough bits...
+        index = startingIndex
+        while (index-startingIndex) < nBits:
+            p = Packet(inputBitStream,index,depth)
+            index = p.getLastUsedBitIndex() + 1 # move on one bit for start of next packet...
+            self.subPackets.append(p)
+        return index-1 # -1 because we are reporting characters used.. (ie. p.getLastUsedBitIndex() )
+
+    def eatNSubPackets(self,inputBitStream,startingIndex,nSubpackets,depth):
+        index = startingIndex
+        for s in range(nSubpackets):
+            p = Packet(inputBitStream,index,depth)
+            index = p.getLastUsedBitIndex() + 1 # advance by one
+            self.subPackets.append(p)
+        return index - 1 # report chars used, not next position
+
+    def eatLiteralFromStream(self,inputBitStream,startingIndex):
+        self.number=-1
+        self.endPosInBitStream = -1
+        index = startingIndex
+        literalBitString = ""
+        # This is the only bit not tested in any of my test cases!
+        # Count forward groups of 5 bits until lead bit is a 0
+
+        # Trying to get string out of bytes,
+
+        while inputBitStream[index] == '1':
+            literalBitString += inputBitStream[index+1] #ignore leading 0
+            literalBitString += inputBitStream[index+2]
+            literalBitString += inputBitStream[index+3]
+            literalBitString += inputBitStream[index+4]
+            index = index + 5
+        # Last group
+        literalBitString += inputBitStream[index+1] #ignore leading 0
+        literalBitString += inputBitStream[index+2]
+        literalBitString += inputBitStream[index+3]
+        literalBitString += inputBitStream[index+4]
+        self.number = int(literalBitString,2)
+        self.endPosInBitStream = index+4
+
+    def getLastUsedBitIndex(self):
+        #We also talk about how many bit have been used
+        if len(self.subPackets) > 0:
+            return self.subPackets[-1].getLastUsedBitIndex()
+        else:
+            return self.endPosInBitStream
+
+    def sumVersion(self):
+
+        sumVer = self.packetVersion
+        
+        if len(self.subPackets) > 0:
+            for i in self.subPackets:
+                sumVer = sumVer +i.sumVersion()
+
+        return sumVer
+    
+    def evaluate(self):
+        evalSub =[]
+        evalSub.extend(map(lambda x: x.evaluate(),self.subPackets))
+        if self.typeID == 0:
+            #Packets with type ID 0 are sum packets - their value is the sum of the values of their sub-packets. If they only have a single sub-packet, their value is the value of the sub-packet.
+            return sum(evalSub)
+        elif self.typeID == 1:
+            #Packets with type ID 1 are product packets - their value is the result of multiplying together the values of their sub-packets. If they only have a single sub-packet, their value is the value of the sub-packet.
+            product = 0
+            if len(evalSub) > 0:
+                product = 1
+            for i in evalSub:
+                product = product * i
+            return product
+        elif self.typeID == 2:
+            #Packets with type ID 2 are minimum packets - their value is the minimum of the values of their sub-packets.
+            return min(evalSub)
+        elif self.typeID == 3:
+            #Packets with type ID 3 are maximum packets - their value is the maximum of the values of their sub-packets.
+            return max(evalSub)                
+        elif self.typeID == 4:
+            # literal
+            return self.number
+        elif self.typeID == 5:
+            #Packets with type ID 5 are greater than packets - their value is 1 if the value of the first sub-packet is greater than the value of the second sub-packet; otherwise, their value is 0.
+            # These packets always have exactly two sub-packets
+            assert(len(evalSub)==2)
+            if evalSub[0] > evalSub[1]: 
+                return 1
+            else:
+                return 0
+        elif self.typeID == 6:
+            # Packets with type ID 6 are less than packets - their value is 1 if the value of the first sub-packet is less than the value of the second sub-packet; otherwise, their value is 0. 
+            # These packets always have exactly two sub-packets.
+            assert(len(evalSub)==2)
+            if evalSub[0] < evalSub[1]:
+                return 1
+            else:
+                return 0
+        elif self.typeID == 7:
+            # Packets with type ID 7 are equal to packets - their value is 1 if the value of the first sub-packet is equal to the value of the second sub-packet; otherwise, their value is 0.
+            # These packets always have exactly two sub-packets.
+            assert(len(evalSub)==2)
+            if evalSub[0]== evalSub[1]:
+                return 1
+            else:
+                return 0
+        else:
+            assert(False)
+            return -1
+
+    def toString(self):
+        evalSub =[]
+        evalSub.extend(map(lambda x: x.toString(),self.subPackets))
+        if self.typeID == 0:
+            #Packets with type ID 0 are sum packets - their value is the sum of the values of their sub-packets. If they only have a single sub-packet, their value is the value of the sub-packet.
+            sum = "("
+            for i in evalSub:
+                sum = (sum + i +" + ")
+            return sum[:len(sum)-3] + ")"
+        elif self.typeID == 1:
+            #Packets with type ID 1 are product packets - their value is the result of multiplying together the values of their sub-packets. If they only have a single sub-packet, their value is the value of the sub-packet.
+            product = "("
+            for i in evalSub:
+                product = (product + i + " * ")
+            return product[:len(product) - 3] +")"
+        elif self.typeID == 2:
+            #Packets with type ID 2 are minimum packets - their value is the minimum of the values of their sub-packets.
+            #return "min[".join(map(str,evalSub)) +"]"
+            return "min"+str(evalSub).replace("'",'')+""
+            #return "1/0"
+        elif self.typeID == 3:
+            #Packets with type ID 3 are maximum packets - their value is the maximum of the values of their sub-packets.
+            return "max"+str(evalSub).replace("'",'')+""
+            #return "1/0"
+        elif self.typeID == 4:
+            # literal
+            return str(self.number)
+        elif self.typeID == 5:
+            #Packets with type ID 5 are greater than packets - their value is 1 if the value of the first sub-packet is greater than the value of the second sub-packet; otherwise, their value is 0.
+            # These packets always have exactly two sub-packets
+            return "("+ evalSub[0] +">"+ evalSub[1]+")"
+            #return "1/0"
+                
+        elif self.typeID == 6:
+            # Packets with type ID 6 are less than packets - their value is 1 if the value of the first sub-packet is less than the value of the second sub-packet; otherwise, their value is 0. 
+            # These packets always have exactly two sub-packets.
+            assert(len(evalSub)==2)
+            return "("+ evalSub[0] +"<"+ evalSub[1]+")"
+            #return "1/0"
+
+        elif self.typeID == 7:
+            # Packets with type ID 7 are equal to packets - their value is 1 if the value of the first sub-packet is equal to the value of the second sub-packet; otherwise, their value is 0.
+            # These packets always have exactly two sub-packets.
+            assert(len(evalSub)==2)
+            return "("+ evalSub[0] +"=="+ evalSub[1]+")"
+            #return "1/0"
+        else:
+            assert(False)
+            return ""
+
+            
+
+def hexStringIntoBitString(line):
+    
+    struct=""
+    for d in line.strip():
+        if d == '0': struct +='0000'
+        if d == '1': struct +='0001'
+        if d == '2': struct +='0010'
+        if d == '3': struct +='0011'
+        if d == '4': struct +='0100'
+        if d == '5': struct +='0101'
+        if d == '6': struct +='0110'
+        if d == '7': struct +='0111'
+        if d == '8': struct +='1000'
+        if d == '9': struct +='1001'
+        if d == 'A': struct +='1010'
+        if d == 'B': struct +='1011'
+        if d == 'C': struct +='1100'
+        if d == 'D': struct +='1101'
+        if d == 'E': struct +='1110'
+        if d == 'F': struct +='1111'
+
+    return struct
+
+def processInputFile(filePath):
+    bitStream =""
+    heights = ""
+    if os.path.exists(filePath):
+        f = open(filePath, "r")
+        for x in f:
+            bitStream = hexStringIntoBitString(x)
+    else:
+        print("File "+filePath+" not found")
+    return bitStream
+
+
+def EatBits(bitString,packets,processed):
+
+    # Make a new packet
+    packets.append(Packet(bitString,processed,0))
+    # This will make a packet with zero or more sub-packets
+    # and use up some or all of the bitString
+    processed = packets[-1].getLastUsedBitIndex()
+
+    return processed
+
+def GetPackets(bitString):
+    print(bitString)
+    packets = []
+
+    targetToProcess = len(bitString) # ignore up to 6 final characters
+    processed = 0
+
+    while processed < targetToProcess:
+        if not '1' in bitString[processed:targetToProcess]:
+            print("Not attempt to process tail {}".format(bitString[processed:targetToProcess]))
+            break
+        processed = EatBits(bitString,packets,processed)
+        processed = processed+1 # next char to process
+        
+    assert(len(packets) == 1)
+    versionSum = packets[0].sumVersion()
+    print("Version sum is {}".format(versionSum))
+
+    toString = packets[0].toString()
+    print("Packet eval is {}".format(toString))
+
+    answer = packets[0].evaluate()
+    print("Evaluated answer is {}".format(answer))
+
+    return answer
+
+def mainTask():
+    t1_start = perf_counter()  
+    
+    #assert(GetPackets(hexStringIntoBitString("D2FE28")) == 2021)
+    
+    #assert(GetPackets("1101001011111110101111111000101000") == 518117)
+
+    #C200B40A82 finds the sum of 1 and 2, resulting in the value 3.
+    #assert(GetPackets(hexStringIntoBitString("C200B40A82")) == 3)
+    # 04005AC33890 finds the product of 6 and 9, resulting in the value 54.
+    #assert(GetPackets(hexStringIntoBitString("04005AC33890")) == 54)
+    # 880086C3E88112 finds the minimum of 7, 8, and 9, resulting in the value 7.
+    #assert(GetPackets(hexStringIntoBitString("880086C3E88112")) == 7)
+    # CE00C43D881120 finds the maximum of 7, 8, and 9, resulting in the value 9.
+    #assert(GetPackets(hexStringIntoBitString("CE00C43D881120")) == 9)
+    # D8005AC2A8F0 produces 1, because 5 is less than 15.
+    #assert(GetPackets(hexStringIntoBitString("D8005AC2A8F0")) == 1)
+    # F600BC2D8F produces 0, because 5 is not greater than 15.
+    #assert(GetPackets(hexStringIntoBitString("F600BC2D8F")) == 0)
+    # 9C005AC2F8F0 produces 0, because 5 is not equal to 15.
+    #assert(GetPackets(hexStringIntoBitString("9C005AC2F8F0")) == 0)
+    # 9C0141080250320F1802104A08 produces 1, because 1 + 3 = 2 * 2.
+    #assert(GetPackets(hexStringIntoBitString("9C0141080250320F1802104A08")) == 1)
+
+    # A0016C880162017C3686B18A3D4780 is an operator packet that contains an operator packet that contains an operator packet that contains five literal values; 
+    #assert(GetPackets(hexStringIntoBitString("A0016C880162017C3686B18A3D4780")) == 1)
+
+    input_path = "C:\\Users\\gibbens\\Documents\\Arduino\\AdventOfCode2021\\16a\\tool_src\\input.txt"
+    bitString = processInputFile(input_path)
+    GetPackets(bitString)
+    
+
+    t1_stop = perf_counter() 
+    print("Elapsed time for main is {}".format(t1_stop-t1_start)) 
+
+if __name__ == "__main__":
+    mainTask()
---
+++ 17a/tool_src/advent.py
@@ -0,0 +1,222 @@
+import os
+from time import perf_counter
+import math
+
+def makeTarget(x1,x2,y1,y2):
+    target = []
+    if x1 < x2:
+        target.append(x1)
+        target.append(x2)
+    else:
+        target.append(x2)
+        target.append(x1)
+
+    if y1 > y2:
+        target.append(y1)
+        target.append(y2)
+    else:
+        target.append(y2)
+        target.append(y1)
+    
+    print("Made target region {}..{}, {}..{}".format(target[0],target[1],target[2],target[3]))
+    
+    return target
+
+def isInTarget(target,x,y):
+
+    if x >= target[0]:
+        if x <= target[1]:
+            if y >= target[3]:
+                if y <= target[2]:
+                    return True    
+    return False
+
+def shotCouldStillHitTarget(target,x,y,x_velocity):
+
+    if isInTarget(target,x,y):
+        return True
+
+    # is shot long?
+    isShotLong = False
+    x1 = target[0]
+    x2 = target[1]
+    if x1 < 0 and x2 < 0:
+        # shooting backwards
+        isShotLong = x < x1
+    
+    if x1 > 0 and x2 > 0:
+        # shooting forwards
+        isShotLong = x > x2
+
+    if x1 < 0 and x2 > 0:
+        # target crosses zero
+        if x > x2:
+            isShotLong = True
+        elif x < x1: 
+            isShotLong = True
+    
+    if isShotLong:
+        return False
+    
+    # X can be before the target area, or directly over the target area, dropping vertically
+    y1 = target[2]
+    y2 = target[3]
+    if x_velocity == 0:
+        if y > y2 and y > y1:
+            # are we above the target?
+            if x >= x1 and x <= x2:
+                return True
+            else:
+                return False
+        else:
+            # fell below target
+            return False    
+
+    return True
+
+
+def getTrajectory(forward,upward,target):
+    assert(forward >= 0)
+    #The probe's x,y position starts at 0,0.
+    steps = []
+    xPos = 0
+    yPos = 0
+    stepVelocityX = forward
+    stepVelocityY = upward
+    steps.append((xPos,yPos))
+    #print("Step ({},{})".format(xPos,yPos))
+    while shotCouldStillHitTarget(target,xPos,yPos,stepVelocityX):
+        
+
+        if isInTarget(target,xPos,yPos):
+            break
+
+        # The probe's x position increases by its x velocity.
+        xPos = xPos + stepVelocityX
+        # The probe's y position increases by its y velocity.
+        yPos = yPos + stepVelocityY
+        # Due to drag, the probe's x velocity changes by 1 toward the value 0; that is, it decreases by 1 if it is greater than 0, 
+        if stepVelocityX > 0:
+            stepVelocityX = stepVelocityX - 1
+        # increases by 1 if it is less than 0, or does not change if it is already 0.
+        if stepVelocityX < 0:
+            stepVelocityX = stepVelocityX + 1        
+        # Due to gravity, the probe's y velocity decreases by 1.
+        stepVelocityY = stepVelocityY - 1
+
+        steps.append((xPos,yPos))
+        #print("Step ({},{})".format(xPos,yPos))
+        #assert(len(steps) < 10000)
+        #if (len(steps) > 10):
+        #    return steps
+    return steps
+
+def doesAnyStepHitTarget(steps,target):
+    for step in steps:
+        if isInTarget(target,step[0],step[1]):
+            #print("step {} hit target".format(step))
+            return True
+    
+    #print("no steps hit target")
+    return False
+
+def printTrajectoryAndReturnMaxY(steps,target):
+    maxStepY = 0
+    minX = 0
+    maxX = 0
+    minY = 0
+    maxY = 0
+    for step in steps: 
+        if step[0] > maxX:
+            maxX = step[0]
+        if step[0] < minX:
+            minX = step[0]
+        if step[1] > maxY:
+            maxY = step[1]
+        if step[1] < minY:
+            minY = step[1]
+        if step[1] > maxStepY:
+            maxStepY = step[1]
+
+    #print("Trajectory max heigh achieved {}".format(maxStepY))
+
+    if target[0]-1 < minX:
+        minX =target[0]-1
+    if target[1]+1 > maxX:
+        maxX =target[1]+1
+    if target[3]-1 < minY:
+        minY =target[3]-1
+    if target[2]+1 > maxY:
+        maxY =target[2]+1
+    
+    for y in range(maxY,minY,-1):
+        row = ""
+        for x in range(minX,maxX+1):
+            if (x,y) == (0,0):
+                row += "S"
+            elif (x,y) in steps:
+                row += "#"
+            elif isInTarget(target,x,y):
+                row += "T"
+            else:
+                row += "."
+    #    print(row)
+    return maxStepY
+
+
+def mainTask():
+    t1_start = perf_counter()  
+    
+    
+    targetSample = makeTarget(20,30,-10,-5)
+    
+    #stepsSample = getTrajectory(7,2,targetSample)
+    #maxYSample = printTrajectoryAndReturnMaxY(stepsSample,targetSample)
+
+    #stepsSample = getTrajectory(6,3,targetSample)
+    #maxYSample = printTrajectoryAndReturnMaxY(stepsSample,targetSample)
+
+    #stepsSample = getTrajectory(9,0,targetSample)
+    #maxYSample = printTrajectoryAndReturnMaxY(stepsSample,targetSample)
+
+    #stepsSample = getTrajectory(17,-4,targetSample)
+    #maxYSample = printTrajectoryAndReturnMaxY(stepsSample,targetSample)
+
+    #stepsSample = getTrajectory(5,4,targetSample)
+    #maxYSample = printTrajectoryAndReturnMaxY(stepsSample,targetSample)
+
+    #stepsSample = getTrajectory(6,4,targetSample)
+    #maxYSample = printTrajectoryAndReturnMaxY(stepsSample,targetSample)
+
+    #stepsSample = getTrajectory(6,10,targetSample)
+    #maxYSample = printTrajectoryAndReturnMaxY(stepsSample,targetSample)
+
+    target = makeTarget(128,160,-142,-88)
+    #steps = getTrajectory(16,70,target)
+    #maxY = printTrajectoryAndReturnMaxY(steps,target)
+
+    #target = makeTarget(20,30,-10,-5)
+    
+    globalMaxY = 0
+    for xInitial in range(15,18):
+        for yInitial in range(140,150):
+            steps = getTrajectory(xInitial,yInitial,target)
+            hitsTarget = doesAnyStepHitTarget(steps,target)
+            if hitsTarget == True:          
+                maxY = printTrajectoryAndReturnMaxY(steps,target)
+                print("Starting velocities {},{} does hit target after {} steps with max height {}".format(xInitial,yInitial,len(steps),maxY))
+                if maxY > globalMaxY:
+                    globalMaxY = maxY
+            #else:
+                #print("Starting velocities {},{} does not hit target".format(xInitial,yInitial))
+
+    # 190 is too low
+    # 1225
+
+    print("Best height found {}".format(globalMaxY))
+
+    t1_stop = perf_counter() 
+    print("Elapsed time for main is {}".format(t1_stop-t1_start)) 
+
+if __name__ == "__main__":
+    mainTask()
